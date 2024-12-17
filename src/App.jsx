4356c887347d86bd8243c47e639e08b02d0d1a3f
logesh <logeshgopi.madasamy@gmail.com>
2024-12-17 22:08:26 +0530

    feat:Finish Collaboration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 // import Button from "./components/Button";
+import Benefits from "./section/Benefits.jsx";
+import Collaboration from "./section/Collabaration.jsx";
 import Header from "./section/Header.jsx";
 import Hero from "./section/Hero.jsx";
 
@@ -9,6 +11,8 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
       </div>
     </>
   );
