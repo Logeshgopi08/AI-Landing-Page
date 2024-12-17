aba2cee84e456946fa9e09817d4e38d6ec77b28e
logesh <logeshgopi.madasamy@gmail.com>
2024-12-17 15:03:16 +0530

    feat:Finish Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
-import Button from "./components/Button";
+// import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+// import Button from "./components/Button";
 import Header from "./section/Header.jsx";
+import Hero from "./section/Hero.jsx";
 
 function App() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
-      <ButtonGradient />
     </>
   );
 }
