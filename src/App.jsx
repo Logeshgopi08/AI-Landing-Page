f738ecf7a0e0a440c356406fe667ab54d2b3b9bb
logesh <logeshgopi.madasamy@gmail.com>
2024-12-18 15:17:01 +0530

    feat:Finish Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@
 // import Button from "./components/Button";
 import Benefits from "./section/Benefits.jsx";
 import Collaboration from "./section/Collabaration.jsx";
+import Footer from "./section/Footer.jsx";
 import Header from "./section/Header.jsx";
 import Hero from "./section/Hero.jsx";
+import Pricing from "./section/Pricing.jsx";
+import Service from "./section/Service.jsx";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Service />
+        <Pricing />
+        <Footer />
       </div>
+      <ButtonGradient />
     </>
   );
 }
